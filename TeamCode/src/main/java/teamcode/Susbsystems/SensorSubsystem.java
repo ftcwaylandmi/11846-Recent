959cb7ab53043e632914dc0c48c195fd334ef260
jordan <thefizzy247@gmail.com>
2023-11-18 12:57:47 -0500

    INIT COMMIT SPENCER IS DOOKIE

--- TeamCode/src/main/java/teamcode/Susbsystems/SensorSubsystem.java
+++ TeamCode/src/main/java/teamcode/Susbsystems/SensorSubsystem.java
@@ -9,42 +9,4 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class SensorSubsystem {
 
-    public RevColorSensorV3 colorSensor;
-
-    public TouchSensor touchSensor;
-
-    public DigitalChannel magnetSensor;
-
-    public SensorSubsystem(HardwareMap hardwareMap){
-        colorSensor = hardwareMap.get(RevColorSensorV3.class,"colorSensor");
-        touchSensor = hardwareMap.get(TouchSensor.class,"touchSensor");
-        magnetSensor = hardwareMap.get(DigitalChannel.class,"magnetSensor");
-    }
-
-    public int ReturnColor() {
-        return colorSensor.argb();
-
-    }
-
-    public int get() {
-        return colorSensor.red();
-    }
-
-    public int GetColor(){
-        int r = colorSensor.red();
-        int g = colorSensor.green();
-        int b = colorSensor.blue();
-
-        if(r >= g && r >= b){
-            // Red
-            return 2;
-        } else if(g >= b && g >= r){
-            //Green
-            return 1;
-        } else if(b > r && b > g){
-            //blue
-            return 3;
-        }
-        return 1;
-    }
 }
