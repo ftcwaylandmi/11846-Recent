edee38c608f1418d830fad13190ca94b8050ae4a
jdonkey123 <avronko@gmail.com>
2023-11-21 17:11:56 -0500

    The start of Spencer's Auton

--- TeamCode/src/main/java/teamcode/Autons/RedClose.java
+++ TeamCode/src/main/java/teamcode/Autons/RedClose.java
@@ -3,6 +3,7 @@ package teamcode.Autons;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.checkerframework.checker.units.qual.A;
 
@@ -10,3 +11,19 @@ import teamcode.Susbsystems.ArmSubsystem;
 import teamcode.Susbsystems.MecanumSubsystem;
 import teamcode.Susbsystems.SensorSubsystem;
 
+@Autonomous(name = "RedClose", group = "Autons")
+public class RedClose extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        MecanumSubsystem drive = new MecanumSubsystem(hardwareMap);
+        ArmSubsystem arm = new ArmSubsystem(hardwareMap);
+        SensorSubsystem sensor = new SensorSubsystem(hardwareMap);
+
+        if (isStopRequested()) return;
+
+
+    }
+}
